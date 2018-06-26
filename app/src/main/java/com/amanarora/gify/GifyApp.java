bf86d200ea3c45c1b43f36d7660195fd63d91ee5
Aman Arora <aman.arora868@gmail.com>
2018-06-27 01:04:55 +0530

    Initial Commit

---
+++ app/src/main/java/com/amanarora/gify/GifyApp.java
@@ -0,0 +1,12 @@
+package com.amanarora.gify;
+
+import android.app.Application;
+
+public class GifyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
